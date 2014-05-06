d861f82872775ae3ea16b35b4d527f31fd3deeb3
David Czarnecki <me@davidczarnecki.com>
2014-05-05 21:48:38 -0400

    Adding tests for CompetitionRankingLeaderboard reverse option

--- test/leaderboard/__init__.py
+++ test/leaderboard/__init__.py
@@ -2,6 +2,7 @@ import unittest
 from leaderboard_test import LeaderboardTest
 from tie_ranking_leaderboard_test import TieRankingLeaderboardTest
 from competition_ranking_leaderboard_test import CompetitionRankingLeaderboardTest
+from reverse_competition_ranking_leaderboard_test import ReverseCompetitionRankingLeaderboardTest
 
 
 def all_tests():
@@ -9,4 +10,5 @@ def all_tests():
     suite.addTest(unittest.makeSuite(LeaderboardTest))
     suite.addTest(unittest.makeSuite(TieRankingLeaderboardTest))
     suite.addTest(unittest.makeSuite(CompetitionRankingLeaderboardTest))
+    suite.addTest(unittest.makeSuite(ReverseCompetitionRankingLeaderboardTest))
     return suite
